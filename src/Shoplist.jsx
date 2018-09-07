fbdad7036bbdda7a694f043efc72ec4bd350fb85
Anna Samoylenko <asamoylenko@ioc.gov.ua>
2018-09-07 11:25:10 +0300

    empty input after adding

--- src/Shoplist.jsx
+++ src/Shoplist.jsx
@@ -8,26 +8,40 @@ class Shoplist extends React.Component {
     super(props);
     this.state = {
       buyItems : ["dress","skirt","hat"],
-      newItemInput : ""
+      newItemInput : "",
+      message:"",
+      
     }
   }
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value});
+  }
+
 
  addItem =(e) =>{
-  
     e.preventDefault()
     const {buyItems} =this.state
     const newItem = this.state.newItemInput;
-    this.setState({
-      buyItems : [...buyItems, newItem],
-
-      
-    })}
+    // const isOntheList = isOntheList.includes(newItem);
+    
+    // if(isOntheList){
+    //   this.setState({
+    //     message : "This is item is already on the list"
+    //   })
+    // }
+    // else{newItem !== "" && this.setState({
+    //   buyItems : [...buyItems, newItem]
+    //   })
+    // }
+    newItem !== "" && this.setState({
+      buyItems : [...buyItems, newItem]
+      })
+    this.addForm.reset();
+  }
   
-    handleUserInput = (e) => {
-      const name = e.target.name;
-      const value = e.target.value;
-      this.setState({[name]: value});
-    }
+
 
   render() {
     const {title} = this.props;
@@ -37,13 +51,14 @@ class Shoplist extends React.Component {
          <header>
             <img src={shop} alt=""/>
             <h1>{title}</h1>
-            <form className="form-inline" onSubmit={(e)=>this.addItem(e)}>
+            <form className="form-inline" onSubmit={(e)=>this.addItem(e)} ref={input => this.addForm =input} >
               <div className ="form-group">
                 <label htmlFor="addItem" className="sr-only">Add New Item</label>
                 <input type="text" name="newItemInput" className ="form-control" value={this.state.addItem} onChange={this.handleUserInput}/>
               </div>
               <button className="btn btn-primary">Add</button>
          </form>
+         {this.state.message}
          </header>
          
          <div className="content">

b769c9aa72addeb39f64674a61bc1354a2c0da2e
Anna Samoylenko <asamoylenko@ioc.gov.ua>
2018-09-11 16:45:04 +0300

    added DELETE buttons

--- src/Shoplist.jsx
+++ src/Shoplist.jsx
@@ -1,68 +1,93 @@
 import React from 'react';
-import shop from './shop.svg';
+
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
+import { Button } from "react-bootstrap";
+
 
 class Shoplist extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      buyItems : ["dress","skirt","hat"],
-      newItemInput : "",
-      message:"",
-      
+      buyItems : ["dress","skirt","hat"], 
+    
+      message : " " 
     }
   }
-  handleUserInput = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({[name]: value});
-  }
 
-
- addItem =(e) =>{
-    e.preventDefault()
-    const {buyItems} =this.state
-    const newItem = this.state.newItemInput;
-    // const isOntheList = isOntheList.includes(newItem);
+  addItem = (e) => {
+    e.preventDefault();
+    const {buyItems} = this.state;
+    const newItem = this.newItem.value; 
+    const hasValue = this.state.buyItems.includes(newItem)
     
-    // if(isOntheList){
-    //   this.setState({
-    //     message : "This is item is already on the list"
-    //   })
-    // }
-    // else{newItem !== "" && this.setState({
-    //   buyItems : [...buyItems, newItem]
-    //   })
-    // }
-    newItem !== "" && this.setState({
-      buyItems : [...buyItems, newItem]
+    if(hasValue ){
+      this.setState({
+        message : "This item has already on the list " 
       })
-    this.addForm.reset();
+    }else{
+      newItem !== "" && this.setState({
+        buyItems : [...this.state.buyItems, newItem],
+        message : "" 
+      })
+    }
+    this.newForm.reset();
   }
   
 
+  removeItem = (Item) => {
+    const {buyItems} = this.state;
+    const newBuyItems = buyItems.filter(Buyitem=>{
+      return Buyitem !== Item;
+    })
+    this.setState({
+     buyItems : [...newBuyItems]
+    })
+    if (newBuyItems.length === 0){
+      this.setState({
+        message : "You delete all the items",
+        buyItems : []
+        
+       })
+
+     }
+   }
+
+   removeAll = () => 
+   this.setState({
+    message : "You delete all the items",
+    buyItems : []
+    
+   })
+
+
 
   render() {
-    const {title} = this.props;
-    const {buyItems} = this.state;
+    const {buyItems, message} = this.state;
+    
+
     return (
+      
+
       <div className="shopping_list">
          <header>
-            <img src={shop} alt=""/>
-            <h1>{title}</h1>
-            <form className="form-inline" onSubmit={(e)=>this.addItem(e)} ref={input => this.addForm =input} >
+            <img src= {this.props.src}/>
+            <h1>{this.props.title}</h1>
+            <form className="form-inline" onSubmit={this.addItem} ref={input=>this.newForm = input} >
               <div className ="form-group">
                 <label htmlFor="addItem" className="sr-only">Add New Item</label>
-                <input type="text" name="newItemInput" className ="form-control" value={this.state.addItem} onChange={this.handleUserInput}/>
+                <input type="text" name="newItemInput" className ="form-control" ref={input=>this.newItem = input}/>
               </div>
               <button className="btn btn-primary">Add</button>
          </form>
-         {this.state.message}
          </header>
          
-         <div className="content">
-            <table className="table">
+        
+         
+        {message !== "" &&  <p className="text-danger">{message} </p> }
+
+        {buyItems.length !== 0 &&   <div className="content"> 
+              <table className="table">
                 <thead>
                     <tr>
                         <th>#</th>
@@ -71,16 +96,25 @@ class Shoplist extends React.Component {
                     </tr>
                   </thead>
                   <tbody>
-                      {buyItems.map((Item,i)=>
-                      (<tr key={Item}>
-                      <th scope="row">1</th>
-                      <td>{Item}</td>
-                      <td>Button</td>
-                    </tr>)
-                  )} 
+                    {buyItems.map((Item,i)=> {
+                     return (<tr key={Item}>
+                          <th>{i+1}</th>
+                          <th>{Item}</th>
+                          <th><Button onClick = {e => this.removeItem(Item)}>Delete</Button></th>
+                      </tr>)
+                    })}
                 </tbody>
+                <tfoot>
+                  <td colSpan="2">&nbsp;</td>
+                  <td>
+                    <Button className="btn btn-primary" onClick={(e)=>this.removeAll()}>Remove all</Button>
+                  </td>
+                </tfoot>
               </table>
-         </div>
+            </div>
+            }  
+            
+         
          
   </div>
    );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Shoplist from './Shoplist';
 import registerServiceWorker from './registerServiceWorker';
+import shop from './shop.svg';
 
-ReactDOM.render(<Shoplist title="Shopping list" />, document.getElementById('root'));
+
+ReactDOM.render(<Shoplist 
+    title="Fancy shopping list" 
+    src={shop}
+    numbers = {[1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}/>, document.getElementById('root'));
 registerServiceWorker();
